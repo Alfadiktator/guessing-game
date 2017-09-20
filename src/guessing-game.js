621a9db7d632bd28f7d7165b4c77eacb21b7252d
Alfadiktator <alfadiktator@gmail.com>
2017-09-20 22:13:41 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    
+    constructor() {
+        this.min=0;
+        this.max=0;
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+		return (~~((this.min+this.max)/2));
     }
 
     lower() {
-
+		this.max=(~~((this.min+this.max)/2))-1;
     }
 
     greater() {
-
+		this.min=(~~((this.min+this.max)/2))+1;
     }
 }
 
